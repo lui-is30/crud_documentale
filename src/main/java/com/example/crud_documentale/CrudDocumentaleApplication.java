94070e45aca76b654eded7c11deffaf489549d8f
lui-is30 <luisitohblas@gmail.com>
2025-04-17 20:48:29 +0200

    setup progetto

---
+++ src/main/java/com/example/crud_documentale/CrudDocumentaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_documentale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDocumentaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDocumentaleApplication.class, args);
+	}
+
+}
